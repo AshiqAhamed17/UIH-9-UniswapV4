5e7c7d588b315102d3bdfa3ebaad0e1ab08f2846
AshiqAhamed17 <aashiq121205@gmail.com>
2026-05-01 18:47:48 +0530

    Update PointsHook

--- first-hook/src/PointsHook.sol
+++ first-hook/src/PointsHook.sol
@@ -2,11 +2,91 @@
 pragma solidity ^0.8.26;
 
 import {BaseHook} from "v4-hooks-public/src/base/BaseHook.sol";
+import {ERC1155} from "solmate/src/tokens/ERC1155.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+import {PoolId} from "v4-core/types/PoolId.sol";
+import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
+import {SwapParams} from "v4-core/types/PoolOperation.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+
+contract PointsHook is BaseHook, ERC1155 {
+
+    constructor (IPoolManager _manager) BaseHook(_manager) {}
 
 
-contract PointsHook {
+    function getHookPermissions() public pure override returns(Hooks.Permissions memory) {
+        return
+            Hooks.Permissions({
+                beforeInitialize: false,
+                afterInitialize: false,
+                beforeAddLiquidity: false,
+                beforeRemoveLiquidity: false,
+                afterAddLiquidity: false,
+                afterRemoveLiquidity: false,
+                beforeSwap: false,
+                afterSwap: true,
+                beforeDonate: false,
+                afterDonate: false,
+                beforeSwapReturnDelta: false,
+                afterSwapReturnDelta: false,
+                afterAddLiquidityReturnDelta: false,
+                afterRemoveLiquidityReturnDelta: false
+            });
+    }
 
+    function uri(uint256) public view virtual override returns (string memory) {
+        return "https://api.example.com/token/{id}";
+    }
 
+    function _afterSwap(
+        address,
+        PoolKey calldata key,
+        SwapParams calldata swapParams,
+        BalanceDelta delta,
+        bytes calldata hookData
+    ) internal override returns(bytes4, int128) {
+
+        // Check if the pair is ETH-Token pair, note: currency0 will always be the native toke (ETH)
+        if(!key.currency0.isAddressZero()) return (this.afterSwap.selector, 0);
+
+        // Check if user is buying Token with ETH not vice-versa
+        if(!swapParams.zeroForOne) return (this.afterSwap.selector, 0);
+
+        // Mint points equal to 20% of the amount of ETH they spent
+        // Since its a zeroForOne swap:
+        // if amountSpecified < 0:
+        //      this is an "exact input for output" swap
+        //      amount of ETH they spent is equal to |amountSpecified|
+        // if amountSpecified > 0:
+        //      this is an "exact output for input" swap
+        //      amount of ETH they spent is equal to BalanceDelta.amount0()
+
+        uint256 ethSpendAmount = uint256(int256(-delta.amount0()));
+        uint256 pointsForSwap = ethSpendAmount / 5;
+
+        _assignPoints(key.toId(), hookData, pointsForSwap);
+
+        return (this.afterSwap.selector, 0);
+    }
+
+    function _assignPoints(
+        PoolId poolId,
+        bytes calldata hookData,
+        uint256 points
+    ) internal {
+        // If no hookData is passed in, no points will be assigned to anyone
+        if(hookData.length == 0) return;
+
+        //Extract user address from Hookdata Note: we cannot extract user address by msg.sender becoz msg.sender is v4 router
+        address user = abi.decode(hookData, (address));
+
+        if(user == address(0)) return;
+
+        // Mint points to the user
+        uint256 poolIdUint = uint256(PoolId.unwrap(poolId));
+        _mint(user, poolIdUint, points, "");
+    }
 
 
 }
